a1fb0a305aef3d060e03388cd232a08a4c53244e
PavuluriHemaChowdary <pavulurihema5@gmail.com>
2022-10-24 10:14:07 +0530

    new:HookExamples

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,11 +1,29 @@
-import CricScore from "./components/CricScore";
 
-function App() {
+import React from 'react';
+import BlogContextDemo from './components/BlogContextDemo'
+
+const blogInfo = {
+  React: {
+    post: "Learn useContext Hooks",
+    author: "Tony Stark"
+  },
+  NodeJS: {
+    post: "Node Commands",
+    author: "Steve Rogers"
+  }
+};
+
+export const BlogContext  = React.createContext(blogInfo);
+
+export default function App() {
   return (
-   <div> 
-        <CricScore />
-   </div>
+    <div className="App">
+      <div>
+      <h1>Hello Hema</h1>
+      <BlogContext.Provider value={blogInfo}>
+           <BlogContextDemo />
+      </BlogContext.Provider>
+      </div>
+    </div>
   );
 }
-
-export default App;
