626f59a0cac2d9d88aa66da759626fe74169cc4b
PavuluriHemaChowdary <pavulurihema5@gmail.com>
2022-10-28 19:41:32 +0530

    components

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,29 +1,8 @@
-
-import React from 'react';
-import BlogContextDemo from './components/BlogContextDemo'
-
-const blogInfo = {
-  React: {
-    post: "Learn useContext Hooks",
-    author: "Tony Stark"
-  },
-  NodeJS: {
-    post: "Node Commands",
-    author: "Steve Rogers"
-  }
-};
-
-export const BlogContext  = React.createContext(blogInfo);
-
-export default function App() {
-  return (
-    <div className="App">
-      <div>
-      <h1>Hello Hema</h1>
-      <BlogContext.Provider value={blogInfo}>
-           <BlogContextDemo />
-      </BlogContext.Provider>
-      </div>
+import AxiosDemo from './components/AxiosDemo';
+export default function App(){
+  return(
+    <div>
+      <AxiosDemo />
     </div>
   );
-}
+  }
